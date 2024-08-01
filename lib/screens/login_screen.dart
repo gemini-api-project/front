bcf701a1c38dabaf1fff7859eb8aa2ff8a0e5c64
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-08-02 02:02:01 +0900

    chore: secure🔐

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
