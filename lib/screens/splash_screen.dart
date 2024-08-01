24fa59afd6ac1612ce560652958409e452b9a39d
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-08-01 23:30:41 +0900

    chore: splash

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -10,7 +10,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(child: Text("asdf")),
     );
   }
