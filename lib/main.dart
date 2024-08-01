88464a07708f97c74f442655894ab8cb98cadb3b
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-08-02 00:15:09 +0900

    chore: appicon and appname

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
